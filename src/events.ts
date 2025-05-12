f424f4ed0926fcf9b0014cf78b9be28b4ac3b756
richie-south <richard.soderman@gmail.com>
2025-05-12 10:37:00 +0200

    feat: events handler

---
+++ src/events.ts
@@ -0,0 +1,30 @@
+type Method = (params: unknown) => void
+
+export function createEvents() {
+  const methods = new Map<string, Array<Method>>()
+
+  const register = (key: string, fn: Method) => {
+    methods.set(key, [...(methods.get(key) ?? []), fn])
+  }
+
+  const deregister = (methodName: string) => {
+    const existed = methods.delete(methodName)
+    return existed
+  }
+
+  const handle = (key: string, payload: unknown) => {
+    const fns = methods.get(key)
+
+    if (Array.isArray(fns)) {
+      fns.map((fn) => {
+        try {
+          fn(payload)
+        } catch (error) {
+          // slient
+        }
+      })
+    }
+  }
+
+  return {register, deregister, handle}
+}
