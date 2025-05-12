132ebed179011e336217b2247ce4e74b9b600bc6
richie-south <richard.soderman@gmail.com>
2025-05-12 10:37:50 +0200

    feat: publisher side

---
+++ src/publisher.ts
@@ -0,0 +1,277 @@
+import {generateUniqId} from './generate-uniq-id'
+import {createRpcHandler} from './rpc'
+import {
+  CallFnMessage,
+  callFnMessage,
+  connectedMessage,
+  ConnectedMessage,
+  ConnectMessage,
+  connectMessage,
+  EmitMessage,
+  emitMessage,
+  Events,
+  pingMessage,
+  PingMessage,
+  PongMessage,
+  pongMessage,
+  responseMessage,
+  ResponseMessage,
+  updateGlobalsMessage,
+  UpdateGlobalsMessage,
+} from './types/post-messages'
+import {Attributes, Available, GetContainer} from './types/types'
+
+function parseGlobals(available: Available) {
+  return Object.fromEntries(
+    Object.entries(available ?? {}).filter(
+      ([key, value]) => typeof value !== 'function',
+    ),
+  )
+}
+
+function setUpIframe(
+  src: string,
+  attributes: Attributes,
+  getContainer?: GetContainer,
+) {
+  const uid = `uid_framecomms_${generateUniqId.rnd()}`
+  const iframe = document.createElement('iframe')
+
+  if (attributes && attributes.iframe) {
+    Object.keys(attributes.iframe).forEach((key) => {
+      iframe.setAttribute(key, attributes.iframe[key])
+    })
+  }
+
+  iframe.setAttribute('frameborder', '0')
+  iframe.setAttribute('src', src)
+  iframe.setAttribute('id', uid)
+
+  const origin = new URL(iframe?.src || '').origin
+
+  const fragment = document.createDocumentFragment()
+  if (getContainer) {
+    fragment.appendChild(
+      getContainer({
+        uid,
+        iframe,
+      }),
+    )
+  } else {
+    fragment.appendChild(iframe)
+  }
+
+  return {
+    fragment,
+    iframe,
+    origin,
+    uid,
+  }
+}
+
+export function createIframe({
+  id,
+  src,
+  attributes,
+  available,
+  container,
+}: {
+  id: string
+  src: string
+  attributes?: Attributes
+  available?: Available
+  container?: GetContainer
+}) {
+  let globals = parseGlobals(available)
+  const {fragment, iframe, origin} = setUpIframe(src, attributes, container)
+  const rpc = createRpcHandler()
+  const subscribers = []
+  let hasIframeLoaded = false
+
+  iframe.addEventListener('load', () => {
+    hasIframeLoaded = true
+    rpc.handle({
+      key: 'iframe-loaded',
+      payload: '',
+    })
+  })
+
+  const onEvent = async (event: MessageEvent<Events>) => {
+    if (!event.data || event.data?.id !== id) {
+      return
+    }
+
+    if (event.data?.type === connectMessage) {
+      const incoming = event.data as unknown as ConnectMessage<string>
+      subscribers.push(incoming.payload)
+
+      const message: ConnectedMessage<typeof globals> = {
+        type: connectedMessage,
+        id,
+        reqId: incoming.reqId,
+        payload: globals,
+      }
+
+      if (hasIframeLoaded) {
+        iframe?.contentWindow?.postMessage(message, origin)
+      } else {
+        rpc.register({
+          key: 'iframe-loaded',
+          onHandle: async () => {
+            iframe?.contentWindow?.postMessage(message, origin)
+          },
+        })
+      }
+
+      return
+    }
+
+    if (!subscribers.includes(event.data['subscriberId'])) {
+      return
+    }
+
+    if (event.data?.type === pongMessage) {
+      const incoming = event.data as unknown as PongMessage
+
+      rpc.handle({
+        key: `${pongMessage}:${incoming.subscriberId}`,
+        payload: '',
+      })
+      return
+    }
+
+    if (event.data?.type === callFnMessage) {
+      const incoming = event.data as unknown as CallFnMessage
+
+      const availableKeys = Object.keys(available)
+      if (availableKeys.includes(incoming.method)) {
+        const handler = available[incoming.method]
+
+        if (typeof handler === 'function') {
+          try {
+            const response = await handler(incoming.payload)
+            const message: ResponseMessage<typeof response> = {
+              type: responseMessage,
+              id,
+              reqId: event.data.reqId,
+              payload: response,
+            }
+            iframe?.contentWindow?.postMessage(message, '*')
+          } catch (error) {}
+        }
+
+        return
+      }
+    }
+
+    if (event.data.type === responseMessage) {
+      const incoming = event.data as unknown as ResponseMessage
+
+      rpc.handle({
+        key: incoming.reqId,
+        payload: incoming.payload,
+      })
+      return
+    }
+
+    if (event.data.type === emitMessage) {
+      const incoming = event.data as unknown as EmitMessage
+      const message: EmitMessage = {
+        type: emitMessage,
+        id,
+        reqId: incoming.reqId,
+        event: incoming.event,
+        subscriberId: incoming.subscriberId,
+        payload: incoming.payload,
+      }
+      iframe?.contentWindow?.postMessage(message, origin)
+      return
+    }
+  }
+
+  const render = (query: string) => {
+    return window.document.querySelector(query).appendChild(fragment)
+  }
+
+  const call = (method: string, payload: any) => {
+    return new Promise((resolve, reject) => {
+      const reqId = generateUniqId.rnd()
+
+      rpc.register({
+        key: reqId,
+        onHandle: async (resurnParams) => {
+          resolve(resurnParams)
+        },
+        onDeregister: () => {
+          reject(new Error(`no reponse to call ${method}`))
+        },
+      })
+
+      const message: CallFnMessage<typeof payload> = {
+        type: callFnMessage,
+        id,
+        reqId,
+        method,
+        payload,
+      }
+
+      iframe?.contentWindow?.postMessage(message, origin)
+    })
+  }
+
+  const ping = () => {
+    setInterval(() => {
+      if (subscribers.length === 0) {
+        return
+      }
+
+      subscribers.map((subscriberId) => {
+        const time = setTimeout(() => {
+          rpc.deregister(`${pongMessage}:${subscriberId}`)
+        }, 1000)
+
+        rpc.register({
+          key: `${pongMessage}:${subscriberId}`,
+          onHandle: async () => {
+            clearTimeout(time)
+            rpc.deregister(`${pongMessage}:${subscriberId}`)
+          },
+        })
+
+        const message: PingMessage = {
+          type: pingMessage,
+          id,
+          reqId: subscriberId,
+        }
+
+        iframe?.contentWindow?.postMessage(message, origin)
+      })
+    }, 40_000)
+  }
+
+  const addAvailable = (newAvailable: Available) => {
+    available = {
+      ...available,
+      ...newAvailable,
+    }
+
+    globals = parseGlobals(available)
+
+    const message: UpdateGlobalsMessage<typeof globals> = {
+      type: updateGlobalsMessage,
+      id,
+      payload: globals,
+    }
+
+    iframe?.contentWindow?.postMessage(message, origin)
+  }
+
+  window.addEventListener('message', onEvent, false)
+  ping()
+
+  return {
+    render,
+    call,
+    addAvailable,
+  }
+}
