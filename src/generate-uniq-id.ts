b73d4e67d92dc0fabd4f1004141f9cf2d1ead34c
richie-south <richard.soderman@gmail.com>
2025-05-12 12:14:24 +0200

    chore: remove String because toFixed already returns string

--- src/generate-uniq-id.ts
+++ src/generate-uniq-id.ts
@@ -1,3 +1,3 @@
 export const getId = () =>
   'FRAMECOMMS_' +
-  String((Math.random() * 100000 + 10000).toFixed(5).split('.').join('_'))
+  (Math.random() * 100000 + 10000).toFixed(5).split('.').join('_')
