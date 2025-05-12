c02b86635343dc77a2b7a8d6412b89d3383a7c7a
richie-south <richard.soderman@gmail.com>
2025-05-12 10:37:21 +0200

    feat: types

---
+++ src/types/types.ts
@@ -0,0 +1,16 @@
+export type Available = Record<
+  string,
+  string | Object | number | ((...args: any[]) => any)
+>
+
+export type Attributes = {
+  iframe?: Record<string, string>
+}
+
+export type GetContainer = ({
+  uid,
+  iframe,
+}: {
+  uid: string
+  iframe: HTMLIFrameElement
+}) => HTMLElement
