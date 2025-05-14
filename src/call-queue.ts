fd8f0336ac594bc8a1b6cb282e208cf63d8b23e9
richie-south <richard.soderman@gmail.com>
2025-05-14 19:53:29 +0200

    feat: call queue handler

---
+++ src/call-queue.ts
@@ -0,0 +1,21 @@
+import {Events} from './types/post-messages'
+
+export function callQueueHandler() {
+  let callQueue: Events[] = []
+
+  const add = (event: Events) => {
+    callQueue.push(event)
+  }
+
+  const flush = (callback: (event: Events) => void) => {
+    callQueue.forEach((event) => {
+      callback(event)
+    })
+    callQueue = []
+  }
+
+  return {
+    add,
+    flush,
+  }
+}
